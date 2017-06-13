d07d83ffa4476a0e9cfac018b79c6c956d3303be
nateday <Nathan.l.day@gmail.com>
2017-06-12 20:50:52 -0600

    ConsoleStore

---
+++ Item.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleStore
+{
+    public class Item
+    {
+        public string Name { get; private set; }
+        public string Description { get; set; }
+        public decimal Price { get; private set; }
+
+        public Item(string name, string description, decimal price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Item: {0}. Price: {1:C}. Description: {2}", Name, Price, Description);
+        }
+    }
+}
